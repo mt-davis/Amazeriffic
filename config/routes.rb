5f020cb661e9e7b5858ee44f78c0c1de99783f79
M.T. Davis <marquese.davis@gmail.com>
2015-05-04 00:27:11 +0000

    Added Index and Faq pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'pages/index'
+  get 'pages/faq'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
